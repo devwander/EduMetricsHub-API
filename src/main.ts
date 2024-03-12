a09dd0de5205bc62290c202a730892eab0d9d109
devwander <josewanderson173@gmail.com>
2024-03-12 00:45:44 -0300

    refactor: cors config

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-    // origin: true,
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
